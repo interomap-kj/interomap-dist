8a1a31215a162171edeab959fcd36c96cca13d46
Lara Klement <klementl@uni-mainz.de>
2026-02-15 15:07:42 +0100

    fix translation

--- _app/immutable/chunks/1.1239d6ec.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.2b4d282f.js";export{t as component};
--- _app/immutable/chunks/2.42afa14b.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.afd01859.js";export{t as component};
