3cb12825f0999a1ece5ab780da4034502207132d
Lara Klement <klementl@uni-mainz.de>
2026-02-27 11:15:04 +0100

    pointer statt mouse

---
+++ _app/immutable/chunks/1.2475d22c.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.3dd831e6.js";export{t as component};
--- _app/immutable/chunks/1.42f84fc6.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.4c4298ed.js";export{t as component};
