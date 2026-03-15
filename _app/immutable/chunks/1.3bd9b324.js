ae7a22c006354ba76064b07c487970034cedfe11
Lara Klement <klementl@uni-mainz.de>
2026-03-15 21:41:09 +0100

    mobile Version ohne Scrollen

--- _app/immutable/chunks/1.3bd9b324.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.03274a79.js";export{t as component};
---
+++ _app/immutable/chunks/1.62d441de.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.31bea0d9.js";export{t as component};
