54d9afd1b26f32229471f2981f5f46e3b86a1264
Lara Klement <klementl@uni-mainz.de>
2026-03-15 22:52:30 +0100

    Handyversion?

---
+++ _app/immutable/chunks/1.4e7f6ef8.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.a9595b53.js";export{t as component};
---
+++ _app/immutable/chunks/2.8f64f588.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.5180731d.js";export{t as component};
