d272077ad164f2302772678e8fab039eb614526d
Lara Klement <klementl@uni-mainz.de>
2026-03-15 23:53:20 +0100

    neuer Versuch Maus + touch

---
+++ _app/immutable/chunks/1.39e978ec.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.ed247f35.js";export{t as component};
--- _app/immutable/chunks/2.ff0dc777.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.92ef1de7.js";export{t as component};
