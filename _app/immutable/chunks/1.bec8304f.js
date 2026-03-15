41c426ff29a9ff73f9c30c7dd77ebc3af500883f
Lara Klement <klementl@uni-mainz.de>
2026-03-16 00:13:10 +0100

    Laptop Tablet Handy

--- _app/immutable/chunks/1.bec8304f.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.e6de496c.js";export{t as component};
---
+++ _app/immutable/chunks/2.d4e682e3.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.56ff6036.js";export{t as component};
