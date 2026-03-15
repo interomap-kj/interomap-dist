ad264520b3c3b54019f548daa55794dbeafa64b6
Lara Klement <klementl@uni-mainz.de>
2026-03-15 23:06:58 +0100

    Handyversion Versuch 15.03.

---
+++ _app/immutable/chunks/1.2844c009.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.acf47ef7.js";export{t as component};
--- _app/immutable/chunks/1.2fa92a28.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.1acfbda0.js";export{t as component};
