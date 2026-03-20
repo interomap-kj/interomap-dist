f979954dfa022e4655d04477bd319cb2ab3b1dff
Lara Klement <klementl@uni-mainz.de>
2026-03-20 21:23:43 +0100

    Update Skala

---
+++ _app/immutable/chunks/1.7c041d99.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.f77b46ff.js";export{t as component};
--- _app/immutable/chunks/1.8cccd208.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.9cc15b4e.js";export{t as component};
