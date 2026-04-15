45f8f1be8736b4ac3c91b6ddc7851f8b1fcbd438
Lara Klement <klementl@uni-mainz.de>
2026-04-15 16:30:07 +0200

    Größe angepasst, ohne Doppelklick

--- _app/immutable/chunks/1.88c6340e.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.599e02dd.js";export{t as component};
--- _app/immutable/chunks/2.4afa5440.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.bc94e2af.js";export{t as component};
