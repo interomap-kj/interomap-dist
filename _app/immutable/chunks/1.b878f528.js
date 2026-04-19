497d374e5df866e1566d3a6820845700b76774b3
Lara Klement <klementl@uni-mainz.de>
2026-04-19 16:12:53 +0200

    Slider verbessert

---
+++ _app/immutable/chunks/1.b878f528.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.6b76eafb.js";export{t as component};
--- _app/immutable/chunks/1.e29c4c9b.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.165dbd26.js";export{t as component};
