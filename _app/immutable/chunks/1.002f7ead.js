6af3ccc15678f7f7ae4ec6751ea3c84c4a397de0
Lara Klement <klementl@uni-mainz.de>
2026-04-19 17:00:39 +0200

    Sliderfarbe

---
+++ _app/immutable/chunks/1.002f7ead.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.6467856c.js";export{t as component};
--- _app/immutable/chunks/1.8756c298.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.9b122d01.js";export{t as component};
