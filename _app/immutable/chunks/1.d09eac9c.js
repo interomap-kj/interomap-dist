af824764ccd3b2f2b3d09c8ca972a0c89d796126
Lara Klement <klementl@uni-mainz.de>
2026-04-22 15:47:35 +0200

    Änderung Skala Stärke

--- _app/immutable/chunks/1.d09eac9c.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.29fe7a3d.js";export{t as component};
---
+++ _app/immutable/chunks/2.ccd1c48e.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.70269716.js";export{t as component};
